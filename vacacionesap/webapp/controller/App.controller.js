67d71ca18244c92914a98cfba39449fabb8d97ce
compdiaz <compdiaz@gmail.com>
2024-06-29 11:26:26 +0000

    29-06-2024

--- vacacionesap/webapp/controller/App.controller.js
+++ vacacionesap/webapp/controller/App.controller.js
@@ -1,12 +1,45 @@
 sap.ui.define([
-    "sap/ui/core/mvc/Controller"
+    'sap/m/MessageToast',
+    'sap/ui/core/Fragment',
+    'sap/ui/core/mvc/Controller',
+    'sap/ui/core/Popup'
 ],
-function (Controller) {
+function (MessageToast, Fragment, Controller, Popup) {
     "use strict";
 
     return Controller.extend("vacacionesap.controller.App", {
-        onInit: function () {
+     	onInit: function(){
+			this.byId("openMenu").attachBrowserEvent("tab keyup", function(oEvent){
+				this._bKeyboard = oEvent.type === "keyup";
+			}, this);
+		},
 
-        }
-    });
+		handlePressOpenMenu: function(oEvent) {
+			var oButton = oEvent.getSource();
+
+			// create menu only once
+			if (!this._menu) {
+				Fragment.load({
+					name: "vacacionesap.fragment.MenuItemEventing",
+					controller: this
+				}).then(function(oMenu){
+					this._menu = oMenu;
+					this.getView().addDependent(this._menu);
+					this._menu.open(this._bKeyboard, oButton, Popup.Dock.BeginTop, Popup.Dock.BeginBottom, oButton);
+				}.bind(this));
+			} else {
+				this._menu.open(this._bKeyboard, oButton, Popup.Dock.BeginTop, Popup.Dock.BeginBottom, oButton);
+			}
+		},
+
+		handleMenuItemPress: function(oEvent) {
+			MessageToast.show("'" + oEvent.getParameter("item").getText() + "' Presionado");
+		},
+
+		handleTextFieldItemPress: function(oEvent) {
+			MessageToast.show("'" + oEvent.getParameter("item").getValue() + "' entered");
+		}
+
+	});
+
 });
